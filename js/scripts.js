bd3dad603283dfd2c1b6f84571dbaae8765f3f85
Jake Edgar <jakeedgar1012@gmail.com>
2022-03-23 15:47:42 -0700

    add radio buttons and more functionality
    Co-authored-by: Brady Diamond <BradyDiamond@users.noreply.github.com>
    Co-authored-by: DiamondsAndRust206 <DiamondsAndRust206@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
 // BUSINESS LOGIC
 
-
-
+function fullName(firstName, lastName) {
+  return firstName + lastName;
+}
 
 // UI LOGIC 
+
+$(document).ready(function() {
+  $("form#name").submit(function(event) {
+    event.preventDefault();
+    const flavor = $("input:radio[name=flavor]:checked").val();
+    const firstName = $("#name1").val();
+    const lastName = $("#name2").val();
+    const result = (firstName + " " + lastName);
+    $("#output").text(result + " - " + flavor.toUpperCase());
+  });
+});
